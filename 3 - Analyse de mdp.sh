b1ee5ae1196db1876345d308e2d0637d8f358351
YoungR971 <146242259+YoungR971@users.noreply.github.com>
2024-05-23 21:09:41 +0000

    Update 3 - Analyse de mdp.sh

--- 3 - Analyse de mdp.sh
+++ 3 - Analyse de mdp.sh
@@ -1,60 +1,63 @@
-#!/bin/bash
+ #### 3 - Analyse de mot de passe ####
 
-#3 - Analyse de mdp
-# vérifie si John the Ripper est déja installé
-if ! command -v john &>/dev/null; then
-    apt install john -y
-fi
 
-# vérifie si le fichier dictionnaire de mdp existe
-fichier_dictionnaire="rockyou.txt"
-if [ ! -f "$fichier_dictionnaire" ]; then
-    echo "Erreur : Le fichier de dictionnaire '$fichier_dictionnaire' n'existe pas."
-    exit 1
-fi
+analyse_mot_de_passe() {
+    echo -e "${bleu}#### Analyse de mot de passe ####${reset}"
+    
+#vérifie si John the Ripper est installé, si non, l'installe
+    if ! command -v john &>/dev/null; then
+        apt install john -y
+    fi
 
-# demande à l'user de saisir le mdp à analyser
-read -sp "Entrez votre mot de passe : " mdp
-echo
+#vérifie la présence duu fichier dictionnaire 
+    fichier_dictionnaire="rockyou.txt"
+    if [ ! -f "$fichier_dictionnaire" ]; then
+        echo "Erreur : Le fichier de dictionnaire '$fichier_dictionnaire' n'existe pas."
+        return  
+    fi
 
-#le mdp est basé sur une politique de mdp pour établir sa robustesse
-#vérifie la longueur du mdp
-if [ ${#mdp} -ge 14 ]; then
-    longueur_mdp=true
-else
-    longueur_mdp=false
-fi
+    read -sp "Entrez votre mot de passe : " mdp
+    echo
 
-# vérifie le nombre de majuscules
-majuscule=$(echo "$mdp" | grep -o '[[:upper:]]' | wc -l)
-if [ $majuscule -ge 2 ]; then
-    majuscule_ok=true
-else
-    majuscule_ok=false
-fi
+#le mot de passe est basé sur une politique de mot de passe pour établir sa robustesse
+#vérifie la longueur du mot de passe
+    if [ ${#mdp} -ge 14 ]; then
+        longueur_mdp=true
+    else
+        longueur_mdp=false
+    fi
 
-# vérifie la présence de caractères spéciaux
-if echo "$mdp" | grep -q '[^[:alnum:]]'; then
-    caractere_spe=true
-else
-    caractere_spe=false
-fi
+    # Vérifie le nombre de majuscules
+    majuscule=$(echo "$mdp" | grep -o '[[:upper:]]' | wc -l)
+    if [ $majuscule -ge 2 ]; then
+        majuscule_ok=true
+    else
+        majuscule_ok=false
+    fi
 
-# détermine la force du mdp
-if $longueur_mdp && $majuscule_ok && $caractere_spe; then
-    echo "Mot de passe fort."
-elif [ ${#mdp} -gt 9 ] && [ $majuscule -gt 1 ] && ! $caractere_spe; then
-    echo "Mot de passe acceptable."
-else
-    echo "Mot de passe faible."
-fi
+    # Vérifie la présence de caractères spéciaux
+    if echo "$mdp" | grep -q '[^[:alnum:]]'; then
+        caractere_spe=true
+    else
+        caractere_spe=false
+    fi
 
-# vérifie si le mot de passe est présent dans le dictionnaire
-if john --wordlist="$fichier_dictionnaire" --stdout | grep -q "$mdp"; then
-    echo "Le mot de passe renseigné est un mot de passe commun."
-fi
+    # Détermine la force du mot de passe
+    if $longueur_mdp && $majuscule_ok && $caractere_spe; then
+        echo -e "${vert}Mot de passe robuste${reset}"
+    elif [ ${#mdp} -gt 9 ] && [ $majuscule -gt 1 ] && ! $caractere_spe; then
+        echo -e "${jaune}Mot de passe acceptable${reset}"
+    else
+        echo -e "${rouge}Mot de passe faible${reset}"
+    fi
 
-#si le mot de passe se trouve dans le dico il est potentiellement compromis
-if $longueur_mdp && $majuscule_ok && $caractere_spe && john --wordlist="$fichier_dictionnaire" --stdout | grep -q "$mdp"; then
-    echo "Mot de passe potentiellement compromis."
-fi
+    # Vérifie si le mot de passe est présent dans le dictionnaire
+    if john --wordlist="$fichier_dictionnaire" --stdout | grep -q "$mdp"; then
+        echo -e "${rouge}Le mot de passe renseigné est un mot de passe commun${reset}"
+    fi
+
+    # Si le mot de passe se trouve dans le dictionnaire, il est potentiellement compromis
+    if $longueur_mdp && $majuscule_ok && $caractere_spe && john --wordlist="$fichier_dictionnaire" --stdout | grep -q "$mdp"; then
+        echo "Mot de passe potentiellement compromis."
+    fi
+}
