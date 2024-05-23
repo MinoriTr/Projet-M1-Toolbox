b533d25caadb62dbf036dd8dbf572c016db04e94
YoungR971 <146242259+YoungR971@users.noreply.github.com>
2024-05-23 21:10:24 +0000

    Update 4 - Test d'authentification.sh

--- 4 - Test d'authentification.sh
+++ 4 - Test d'authentification.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
+   #### 4 - Test d'authentification ####
 
-#test d'authentification 
-#demande les identifiants de connexions ( login user et mot de passe )
 
-read -sp "veuillez entrer votre login : "  login_user
-read -sp "veuillez entrer votre mot de passe : " mdp_user
+test_authentification() {
+    echo -e "${bleu}#### Test d'authentification ####${reset}"
+    
 
- auth_cmd=$(ssh -p 22 $login_user@$target_ip)
+    read -p "Veuillez entrer votre login : " login_user
+    echo
+    read -sp "Veuillez entrer votre mot de passe : " mdp_user
+    echo
+
+ 
+    sshpass -p "$mdp_user" ssh -o StrictHostKeyChecking=no -o BatchMode=yes -o HostKeyAlgorithms=+ssh-rsa $login_user@$ip_cible 'exit'
+
+    if [ $? -eq 0 ]; then
+        echo "Connexion SSH réussie et terminée."
+    else
+        echo "Échec de la connexion SSH."
+    fi
+}
